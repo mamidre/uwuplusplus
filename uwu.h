e48d4179bc0ba3545e8094987399df538a7ce306
Simbrine <simbrine@gmail.com>
2017-03-06 13:18:29 +0200

    add statement type stmt_t, fix braces

--- uwu.h
+++ uwu.h
@@ -1,9 +1,13 @@
 #define UWU_STKLEN 65536
 
-typedef struct uwuplusplus_variable_s
-{
+typedef struct uwuplusplus_variable_s {
 	int type;
 	void *value;
 } variable_t; 
+
+typedef struct uwuplusplus_stmt_s {
+	char *instr;
+	char **args;
+} stmt_t;
 
 extern variable_t vstack[UWU_STKLEN];
